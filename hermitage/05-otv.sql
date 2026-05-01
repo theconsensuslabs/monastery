08e508922880c913f64d8663febe36b13a3fa3e6
Phil Eaton <phil@eatonphil.com>
2026-05-01 13:14:27 -0400

    And more fixes for handling aborts

--- hermitage/05-otv.sql
+++ hermitage/05-otv.sql
@@ -13,12 +13,12 @@ t3: $SHOW_ISOLATION
 
 t1: update test set value = 11 where id = 1;
 t1: update test set value = 19 where id = 2;
-t2: update test set value = 12 where id = 1;
+t2: update test set value = 12 where id = 1;       -- group final; assert ok or t2aborted => error
 t1: commit;
 t3: select * from test where id = 1; -- assert ({1, 11}) or ({1, 12})
-t2: update test set value = 18 where id = 2;
+t2: update test set value = 18 where id = 2;       -- group final; assert ok or t2aborted => error
 t3: select * from test where id = 2; -- assert ({2, 19}) or ({2, 18})
-t2: commit;
+t2: commit;                                         -- group final
 t3: select * from test where id = 2; -- group t3view; assert fresh => ({2, 18}) or stale => ({2, 19})
 t3: select * from test where id = 1; -- group t3view; assert fresh => ({1, 12}) or stale => ({1, 11})
 t3: commit;
--- hermitage/07-pmp-write-predicates.sql
+++ hermitage/07-pmp-write-predicates.sql
@@ -10,7 +10,7 @@ t2: begin;
 t2: $SHOW_ISOLATION;
 
 t1: update test set value = value + 10;
-t2: delete from test where value = 20;
+t2: delete from test where value = 20;             -- group final; assert ok or t2aborted => error
 t1: commit;
-t2: select * from test where value = 20; -- assert error
-t2: commit;
+t2: select * from test where value = 20;           -- group final; assert error
+t2: commit;                                         -- group final
