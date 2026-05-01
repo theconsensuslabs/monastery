960e54277c0c9054d4172d195babdf2553131a9c
Phil Eaton <phil@eatonphil.com>
2026-05-01 13:05:09 -0400

    Fix hermitage/01-g0-write-cycles-dirty-writes.sql

--- hermitage/01-g0-write-cycles-dirty-writes.sql
+++ hermitage/01-g0-write-cycles-dirty-writes.sql
@@ -10,11 +10,11 @@ t2: begin;
 t2: $SHOW_ISOLATION;
 
 t1: update test set value = 11 where id = 1;
-t2: update test set value = 12 where id = 1;
+t2: update test set value = 12 where id = 1;       -- group final; assert t2committed => ok or t2aborted => error
 t1: update test set value = 21 where id = 2;
 t1: commit;
 t1: select * from test; -- assert ({1, 11}, {2, 21})
-t2: update test set value = 22 where id = 2;
+t2: update test set value = 22 where id = 2;       -- group final; assert t2committed => ok or t2aborted => error
 t2: commit;
 t1: select * from test; -- group final; assert t2committed => ({1, 12}, {2, 22}) or t2aborted => ({1, 11}, {2, 21})
 t2: select * from test; -- group final; assert t2committed => ({1, 12}, {2, 22}) or t2aborted => ({1, 11}, {2, 21})
