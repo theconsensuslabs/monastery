50a61e0ffc2d2c8e4dc60f7183f44d7efb53a7df
Phil Eaton <phil@eatonphil.com>
2026-05-02 12:00:03 -0400

    Clean up checks for dirty writes, not dirty reads

--- hermitage/01-g0-write-cycles-dirty-writes.sql
+++ hermitage/01-g0-write-cycles-dirty-writes.sql
@@ -13,7 +13,7 @@ t1: update test set value = 11 where id = 1;
 t2: update test set value = 12 where id = 1;       -- group final; assert t2committed => ok or t2aborted => error
 t1: update test set value = 21 where id = 2;
 t1: commit;
-t1: select * from test; -- assert ({1, 11}, {2, 21})
+t1: select * from test; -- assert ({1, 11}, {2, 21}) or ({1,12}, {2, 21}) # Dirty read is ok.
 t2: update test set value = 22 where id = 2;       -- group final; assert t2committed => ok or t2aborted => error
 t2: commit;
 t1: select * from test; -- group final; assert t2committed => ({1, 12}, {2, 22}) or t2aborted => ({1, 11}, {2, 21})
