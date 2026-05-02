ce25ce486c3bfcc4f59c02d5395c43cee7327aec
Phil Eaton <phil@eatonphil.com>
2026-05-02 14:58:46 -0400

    Fix readme and example

--- examples/dirty-write.sql
+++ examples/dirty-write.sql
@@ -10,8 +10,8 @@ t1: UPDATE shoes SET left_shoe = 'Lin' WHERE shoe_id = 1;
 t2: UPDATE shoes SET left_shoe = 'Carlos' WHERE shoe_id = 1;
 t2: UPDATE shoes SET right_shoe = 'Carlos' WHERE shoe_id = 1;
 t1: UPDATE shoes SET right_shoe = 'Lin' WHERE shoe_id = 1;
-t1: SELECT * FROM shoes; # Inconsistent results here would mean dirty reads not necessarily dirty writes.
-t2: SELECT * FROM shoes; # Inconsistent results here would mean dirty reads not necessarily dirty writes.
+t1: SELECT * FROM shoes; -- # Inconsistent results here would mean dirty reads not necessarily dirty writes.
+t2: SELECT * FROM shoes; -- # Inconsistent results here would mean dirty reads not necessarily dirty writes.
 t1: COMMIT;
 t2: COMMIT;
 t1: SELECT * FROM shoes; -- assert ({Lin, Lin, 1}) or ({Carlos, Carlos, 1})
